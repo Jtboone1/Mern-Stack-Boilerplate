fbf63b4e4e14b217ece43b875dd3b1320315deda
JTBoone <boojarrod@gmail.com>
2020-11-22 18:31:15 -0330

    Created UI for starting screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,39 @@
+import react from "react"
+import { Card, CardContent, makeStyles, Button, TextField } from '@material-ui/core';
 
+const useStyles = makeStyles({
+  root: {
+    minWidth: 475,
+    minHeight: 300
+  }
+})
 
-function App() {
+const App = () =>{
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <h1>Hello from Client</h1>
+    <div style={cardStyle}>
+      <Card className={classes.root}>
+        <CardContent>
+          <div style={{display: "flex", justifyContent: "center", flexDirection: "column"}}>
+            <h1 style={{textAlign: "center"}}>Welcome</h1>
+            <TextField label="Enter Room Code" />
+            <Button color="primary" style={{marginTop: 10}}>
+              Join Room
+            </Button>
+            <h4 style={{textAlign: "center"}}>Or</h4>
+            <Button color="secondary"  style={{marginTop: 5}}>
+              Create New Room
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
+}
+
+const cardStyle = {
+  
 }
 
 export default App;
